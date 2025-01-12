b34d39e4f9763cbf1a3f6ffa8245f61f60e868ad
miguellealdev <miguellealformaggiomarcondes@gmail.com>
2025-01-12 16:42:41 -0300

    ultimas modificações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+   return (
+      <>
+         <h1 className="text-yellow-400 text-xl font-bold text-center p-2">
+            Olá eu sou Miguel
+         </h1>
+         <h2 className="p-4">
+            io mae fiz esse site para comemerar o seu niver e coloquei fotos
+            nossas e muito amor o site e bem simples, te amo muito deus te
+            abensoe e te ilumine.
+         </h2>
+         <div className="flex flex-wrap gap-2 p-4">
+            {/* Div com moldura branca */}
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_eu_ju_predio.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">
+                  {' '}
+                  esse dia foi marcante e muito divertido
+               </p>
+            </div>
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_eu_mc.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">
+                  comi muito nesse dia, lembra da spritk sem gas{' '}
+               </p>
+            </div>
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_eu_museu_2.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">fiquei moscano na foto</p>
+            </div>
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_eu_museu_3.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">
+                  te amo muito muito muito mesmo
+               </p>
+            </div>
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_eu_museu.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">
+                  esse dia foi muito legal
+               </p>
+            </div>
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_ju_estadio.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">fui fotografo</p>
+            </div>
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_ju_predio.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">parabens pra voce</p>
+            </div>
+            <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
+               <img
+                  className="flex w-auto max-h-72 object-cover"
+                  src="images/mae_sozinha_predio.jpg"
+                  alt="Foto eu e mãe no museu"
+               />
+               <p className="text-black text-center">
+                  voce e muito maravilhosa
+               </p>
+            </div>
+         </div>
+      </>
+   );
 }
