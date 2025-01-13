0bf6ba80aaffc48185de690268c3cdce3a4c1b09
miguellealdev <miguellealformaggiomarcondes@gmail.com>
2025-01-13 12:30:14 -0300

    dist

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  reactStrictMode: true,
   trailingSlash: true,
   basePath: '/site-niver-mae',
   assetPrefix: '/site-niver-mae/',
   images: {
     unoptimized: true, // Isso ajuda a evitar problemas com imagens ao exportar
   },
+  distDir: 'dist',
 };
 
 export default nextConfig;
