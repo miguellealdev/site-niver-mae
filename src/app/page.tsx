3d315c639abf5795cce40b412c46a4e026c5a588
miguellealdev <miguellealformaggiomarcondes@gmail.com>
2025-01-13 11:57:58 -0300

    fix Image <- <img>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
          <div className="flex flex-wrap gap-2 p-4">
             {/* Div com moldura branca */}
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_eu_ju_predio.jpg"
                   alt="Foto eu e mãe no museu"
@@ -25,7 +25,7 @@ export default function Home() {
                </p>
             </div>
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_eu_mc.jpg"
                   alt="Foto eu e mãe no museu"
@@ -35,7 +35,7 @@ export default function Home() {
                </p>
             </div>
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_eu_museu_2.jpg"
                   alt="Foto eu e mãe no museu"
@@ -43,7 +43,7 @@ export default function Home() {
                <p className="text-black text-center">fiquei moscano na foto</p>
             </div>
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_eu_museu_3.jpg"
                   alt="Foto eu e mãe no museu"
@@ -53,7 +53,7 @@ export default function Home() {
                </p>
             </div>
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_eu_museu.jpg"
                   alt="Foto eu e mãe no museu"
@@ -63,7 +63,7 @@ export default function Home() {
                </p>
             </div>
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_ju_estadio.jpg"
                   alt="Foto eu e mãe no museu"
@@ -71,7 +71,7 @@ export default function Home() {
                <p className="text-black text-center">fui fotográfo</p>
             </div>
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_ju_predio.jpg"
                   alt="Foto eu e mãe no museu"
@@ -79,7 +79,7 @@ export default function Home() {
                <p className="text-black text-center">parabéns pra você</p>
             </div>
             <div className="flex flex-col px-1 pt-1 pb-9 bg-white">
-               <img
+               <Image
                   className="flex w-auto max-h-72 object-cover"
                   src="images/mae_sozinha_predio.jpg"
                   alt="Foto eu e mãe no museu"
