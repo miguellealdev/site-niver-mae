b5e8de562740099ab4494aece1e713357f38d6ff
miguellealdev <miguellealformaggiomarcondes@gmail.com>
2025-01-13 12:19:45 -0300

    ...

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   basePath: '/site-niver-mae',
+  assetPrefix: '/site-niver-mae/',
   images: {
     unoptimized: true, // Isso ajuda a evitar problemas com imagens ao exportar
   },
